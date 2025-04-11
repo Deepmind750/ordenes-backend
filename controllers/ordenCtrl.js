9d98be5f87d06d3cd8c70723eb7131ee4a88e397
Sistema Barceloneta <sistemabarceloneta@MacBook-Pro-Danny2.local>
2025-04-11 11:13:46 -0400

    📨 Activadas notificaciones por email y SMS desde ordenCtrl

--- controllers/ordenCtrl.js
+++ controllers/ordenCtrl.js
@@ -1,11 +1,13 @@
 const pool = require("../db/db");
+const enviarCorreo = require("../services/email");
 
 const crearOrden = async (req, res) => {
   const { nombre_usuario, telefono, email, oficina, descripcion } = req.body;
 
   try {
-  console.log("Datos recibidos:",req.body);
-      const result = await pool.query(
+    console.log("Datos recibidos:", req.body);
+
+    const result = await pool.query(
       `INSERT INTO ordenes_trabajo
        (nombre_usuario, telefono, email, oficina, descripcion)
        VALUES ($1, $2, $3, $4, $5)
@@ -13,12 +15,15 @@ const crearOrden = async (req, res) => {
       [nombre_usuario, telefono, email, oficina, descripcion]
     );
 
+    // Enviar correo + SMS
+    await enviarCorreo(result.rows[0]);
+
     res.status(201).json({
       mensaje: "Orden registrada con éxito",
       orden: result.rows[0],
     });
   } catch (err) {
-   console.error("error al registrar orden:", err.message); // <-- Esto nos mostrará el error real
+    console.error("error al registrar orden:", err.message);
     res.status(500).json({ error: "Error al registrar la orden" });
   }
 };
