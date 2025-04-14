17af3142ff6b305871ab764506979bc411c0e44e
Sistema Barceloneta <sistemabarceloneta@MacBook-Pro-Danny2.local>
2025-04-14 17:36:27 -0400

    Corrige nombre de index.html y asegura deploy

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Ejecuta tabla si no existe
 require("./db/init");
 
