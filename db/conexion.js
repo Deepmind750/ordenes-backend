11d3884c349c7f5a5e6a428434c981a320377bf9
Sistema Barceloneta <sistemabarceloneta@MacBook-Pro-Danny2.local>
2025-04-15 16:52:34 -0400

    Activar conexión segura para entorno Render

--- db/conexion.js
+++ db/conexion.js
@@ -1,14 +1,11 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-// Detecta si estás en Render (Render automáticamente define esta variable)
-const isRender = process.env.RENDER === 'true';
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: isRender
-    ? { rejectUnauthorized: false }
-    : false // Sin SSL en local
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
